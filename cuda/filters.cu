bdae31b26efaf46641f4c2eeb2be04abeafa708a
answer000123 <answer000123@gmail.com>
2017-09-28 15:02:42 +0200

    grep v1

---
+++ cuda/filters.cu
@@ -0,0 +1,300 @@
+#include <Timer.hpp>
+#include <iostream>
+#include <iomanip>
+
+using LOFAR::NSTimer;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+
+/* Utility function/macro, used to do error checking.
+   Use this function/macro like this:
+   checkCudaCall(cudaMalloc((void **) &deviceRGB, imgS * sizeof(color_t)));
+   And to check the result of a kernel invocation:
+   checkCudaCall(cudaGetLastError());
+*/
+#define checkCudaCall(result) {                                     \
+    if (result != cudaSuccess){                                     \
+        cerr << "cuda error: " << cudaGetErrorString(result);       \
+        cerr << " in " << __FILE__ << " at line "<< __LINE__<<endl; \
+        exit(1);                                                    \
+    }                                                               \
+}
+
+__global__ void rgb2grayCudaKernel(unsigned char *inputImage, unsigned char *grayImage, const int width, const int height)
+{
+
+	float grayPix = 0.0f;
+	uint x = (blockIdx.x * blockDim.x) + threadIdx.x;
+	uint y = (blockIdx.y * blockDim.y) + threadIdx.y;
+	float r = static_cast< float >(inputImage[(y * width) + x]);
+	float g = static_cast< float >(inputImage[(width * height) + (y * width) + x]);
+	float b = static_cast< float >(inputImage[(2 * width * height) + (y * width) + x]);
+	grayPix = (0.3f * r) + (0.59f * g) + (0.11f * b);
+	grayImage[(y * width) + x] = static_cast< unsigned char >(grayPix);
+
+}
+
+void rgb2grayCuda(unsigned char *inputImage, unsigned char *grayImage_gpu, const int width, const int height)
+{
+	
+	dim3 threadsPerBlock(1,1);  // 4 threads
+	dim3 numBlocks(width/threadsPerBlock.x,  /* for instance 512/8 = 64*/
+					height/threadsPerBlock.y); 
+
+	NSTimer kernelTime = NSTimer("kernelTime", false, false);
+
+	//allocation on the Device
+	unsigned char *d_input = 0, *d_output = 0;
+	if(cudaMalloc((void**)&d_input,width*height*sizeof(unsigned char)*3) != cudaSuccess){
+		cout<<"error on allocate device memory"<<endl;
+		exit(-1);
+	};
+	if(cudaMalloc((void**)&d_output,width*height*sizeof(unsigned char))!= cudaSuccess){
+		printf("error on allocate device memory\n");
+		exit(-1);
+	};
+
+	//memory copy from Host to Device 
+	if(cudaMemcpy(d_input,inputImage,width*height*sizeof(unsigned char)*3,cudaMemcpyHostToDevice)
+		!= cudaSuccess){
+		printf("error on memory transfer\n");
+		exit(-2);
+	}
+	
+	kernelTime.start();
+
+	rgb2grayCudaKernel<<<numBlocks,1>>>(d_input,d_output,width, height);
+	cudaDeviceSynchronize();
+
+	kernelTime.stop();
+	
+	if(cudaMemcpy(grayImage_gpu,d_output,width*height*sizeof(unsigned char),cudaMemcpyDeviceToHost)
+		!= cudaSuccess){
+		printf("error on memory transfer\n");
+		exit(-3);
+	}
+	cout << fixed << setprecision(6);
+	cout << "rgb2gray (cpu): \t\t" << kernelTime.getElapsed() << " seconds." << endl;
+	cudaFree(d_input);
+	cudaFree(d_output);
+}
+
+
+
+void rgb2gray(unsigned char *inputImage, unsigned char *grayImage, const int width, const int height) 
+{
+	NSTimer kernelTime = NSTimer("kernelTime", false, false);
+	
+	kernelTime.start();
+	// Kernel
+	for ( int y = 0; y < height; y++ ) 
+	{
+		for ( int x = 0; x < width; x++ ) 
+		{
+			float grayPix = 0.0f;
+			float r = static_cast< float >(inputImage[(y * width) + x]);
+			float g = static_cast< float >(inputImage[(width * height) + (y * width) + x]);
+			float b = static_cast< float >(inputImage[(2 * width * height) + (y * width) + x]);
+
+			grayPix = (0.3f * r) + (0.59f * g) + (0.11f * b);
+
+			grayImage[(y * width) + x] = static_cast< unsigned char >(grayPix);
+		}
+	}
+	// /Kernel
+	kernelTime.stop();
+	
+	cout << fixed << setprecision(6);
+	cout << "rgb2gray (cpu): \t\t" << kernelTime.getElapsed() << " seconds." << endl;
+}
+
+/////////////////////////////////////
+/*
+__global__ void histogram1DCudaKernel
+{
+}
+*/
+
+/*
+void histogram1DCuda
+{
+}
+*/
+
+void histogram1D(unsigned char *grayImage, unsigned char *histogramImage,const int width, const int height, 
+				 unsigned int *histogram, const unsigned int HISTOGRAM_SIZE, 
+				 const unsigned int BAR_WIDTH) 
+{
+	unsigned int max = 0;
+	NSTimer kernelTime = NSTimer("kernelTime", false, false);
+	
+	memset(reinterpret_cast< void * >(histogram), 0, HISTOGRAM_SIZE * sizeof(unsigned int));
+
+	kernelTime.start();
+	// Kernel
+	for ( int y = 0; y < height; y++ ) 
+	{
+		for ( int x = 0; x < width; x++ ) 
+		{
+			histogram[static_cast< unsigned int >(grayImage[(y * width) + x])] += 1;
+		}
+	}
+	// /Kernel
+	kernelTime.stop();
+
+	for ( unsigned int i = 0; i < HISTOGRAM_SIZE; i++ ) 
+	{
+		if ( histogram[i] > max ) 
+		{
+			max = histogram[i];
+		}
+	}
+
+	for ( int x = 0; x < HISTOGRAM_SIZE * BAR_WIDTH; x += BAR_WIDTH ) 
+	{
+		unsigned int value = HISTOGRAM_SIZE - ((histogram[x / BAR_WIDTH] * HISTOGRAM_SIZE) / max);
+
+		for ( unsigned int y = 0; y < value; y++ ) 
+		{
+			for ( unsigned int i = 0; i < BAR_WIDTH; i++ ) 
+			{
+				histogramImage[(y * HISTOGRAM_SIZE * BAR_WIDTH) + x + i] = 0;
+			}
+		}
+		for ( unsigned int y = value; y < HISTOGRAM_SIZE; y++ ) 
+		{
+			for ( unsigned int i = 0; i < BAR_WIDTH; i++ ) 
+			{
+				histogramImage[(y * HISTOGRAM_SIZE * BAR_WIDTH) + x + i] = 255;
+			}
+		}
+	}
+	
+	cout << fixed << setprecision(6);
+	cout << "histogram1D (cpu): \t\t" << kernelTime.getElapsed() << " seconds." << endl;
+}
+
+/////////////////////////////////////
+/*
+__global__ void contrast1DKernel
+{
+}
+*/
+
+/*
+void contrast1DCuda
+{
+}
+*/
+
+void contrast1D(unsigned char *grayImage, const int width, const int height, 
+				unsigned int *histogram, const unsigned int HISTOGRAM_SIZE, 
+				const unsigned int CONTRAST_THRESHOLD) 
+{
+	unsigned int i = 0;
+	NSTimer kernelTime = NSTimer("kernelTime", false, false);
+
+	while ( (i < HISTOGRAM_SIZE) && (histogram[i] < CONTRAST_THRESHOLD) ) 
+	{
+		i++;
+	}
+	unsigned int min = i;
+
+	i = HISTOGRAM_SIZE - 1;
+	while ( (i > min) && (histogram[i] < CONTRAST_THRESHOLD) ) 
+	{
+		i--;
+	}
+	unsigned int max = i;
+	float diff = max - min;
+
+	kernelTime.start();
+	// Kernel
+	for ( int y = 0; y < height; y++ ) 
+	{
+		for (int x = 0; x < width; x++ ) 
+		{
+			unsigned char pixel = grayImage[(y * width) + x];
+
+			if ( pixel < min ) 
+			{
+				pixel = 0;
+			}
+			else if ( pixel > max ) 
+			{
+				pixel = 255;
+			}
+			else 
+			{
+				pixel = static_cast< unsigned char >(255.0f * (pixel - min) / diff);
+			}
+			
+			grayImage[(y * width) + x] = pixel;
+		}
+	}
+	// /Kernel
+	kernelTime.stop();
+	
+	cout << fixed << setprecision(6);
+	cout << "contrast1D (cpu): \t\t" << kernelTime.getElapsed() << " seconds." << endl;
+}
+
+/////////////////////////////////////
+/*
+__global__ void triangularSmoothKernel
+{
+}
+*/
+
+/*
+void triangularSmoothCuda
+{
+}
+*/
+
+void triangularSmooth(unsigned char *grayImage, unsigned char *smoothImage, const int width, const int height,
+					  const float *filter) 
+{
+	NSTimer kernelTime = NSTimer("kernelTime", false, false);
+	
+	kernelTime.start();
+	// Kernel
+	for ( int y = 0; y < height; y++ ) 
+	{
+		for ( int x = 0; x < width; x++ ) 
+		{
+			unsigned int filterItem = 0;
+			float filterSum = 0.0f;
+			float smoothPix = 0.0f;
+
+			for ( int fy = y - 2; fy < y + 3; fy++ ) 
+			{
+				for ( int fx = x - 2; fx < x + 3; fx++ ) 
+				{
+					if ( ((fy < 0) || (fy >= height)) || ((fx < 0) || (fx >= width)) ) 
+					{
+						filterItem++;
+						continue;
+					}
+
+					smoothPix += grayImage[(fy * width) + fx] * filter[filterItem];
+					filterSum += filter[filterItem];
+					filterItem++;
+				}
+			}
+
+			smoothPix /= filterSum;
+			smoothImage[(y * width) + x] = static_cast< unsigned char >(smoothPix);
+		}
+	}
+	// /Kernel
+	kernelTime.stop();
+	
+	cout << fixed << setprecision(6);
+	cout << "triangularSmooth (cpu): \t" << kernelTime.getElapsed() << " seconds." << endl;
+}
+
+
---
+++ cuda/main.cpp
@@ -0,0 +1,155 @@
+#include <CImg.h>
+#include <Timer.hpp>
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <stdio.h>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+using cimg_library::CImg;
+using LOFAR::NSTimer;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::fixed;
+using std::setprecision;
+
+// Constants
+const bool displayImages = false;
+const bool saveAllImages = true;
+const unsigned int HISTOGRAM_SIZE = 256;
+const unsigned int BAR_WIDTH = 4;
+const unsigned int CONTRAST_THRESHOLD = 80;
+const float filter[] = {	1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 
+						1.0f, 2.0f, 2.0f, 2.0f, 1.0f, 
+						1.0f, 2.0f, 3.0f, 2.0f, 1.0f, 
+						1.0f, 2.0f, 2.0f, 2.0f, 1.0f, 
+						1.0f, 1.0f, 1.0f, 1.0f, 1.0f};
+
+extern void rgb2gray(unsigned char *inputImage, unsigned char *grayImage, const int width, const int height);
+//extern void rgb2grayCuda
+
+extern void histogram1D(unsigned char *grayImage, unsigned char *histogramImage, const int width, const int height, unsigned int *histogram, const unsigned int HISTOGRAM_SIZE, const unsigned int BAR_WIDTH);
+//extern void histogram1DCuda 
+
+extern void contrast1D(unsigned char *grayImage, const int width, const int height, unsigned int *histogram, const unsigned int HISTOGRAM_SIZE, const unsigned int CONTRAST_THRESHOLD);
+//extern void contrast1DCuda
+
+extern void triangularSmooth(unsigned char *grayImage, unsigned char *smoothImage, const int width, const int height, const float *filter);
+//extern void triangularSmoothCuda
+
+extern void rgb2grayCuda(unsigned char *inputImage, unsigned char *grayImage, const int width, const int height);
+
+
+int main(int argc, char *argv[]) 
+{
+	if ( argc != 2 ) {
+		cerr << "Usage: " << argv[0] << " <filename>" << endl;
+		return 1;
+	}
+
+	// Load the input image
+	CImg< unsigned char > inputImage = CImg< unsigned char >(argv[1]);
+	if ( displayImages ) {
+		inputImage.display("Input Image");
+	}
+	if ( inputImage.spectrum() != 3 ) {
+		cerr << "The input must be a color image." << endl;
+		return 1;
+	}
+
+	//allocate memory on Host 
+	//unsigned char *buffer = 0;
+	//buffer = (unsigned char*)malloc(inputImage.width()*inputImage.height()*sizeof(unsigned char));
+	
+	//allocation on the Device
+	/*
+	if(cudaMalloc((void**)&d_input,inputImage.width()*inputImage.height()*sizeof(unsigned char)*3) != cudaSuccess){
+		cout<<"error on allocate device memory"<<endl;
+		exit(-1);
+	};
+	if(cudaMalloc((void**)&d_output,inputImage.width()*inputImage.height()*sizeof(unsigned char))!= cudaSuccess){
+		printf("error on allocate device memory\n");
+		exit(-1);
+	};
+
+	//memory copy from Host to Device 
+	if(cudaMemcpy(d_input,inputImage.data(),inputImage.width()*inputImage.height()*sizeof(unsigned char)*3,cudaMemcpyHostToDevice)
+		!= cudaSuccess){
+		printf("error on memory transfer\n");
+		exit(-2);
+	}
+	*/
+	
+	// Convert the input image to grayscale 
+	CImg< unsigned char > grayImage = CImg< unsigned char >(inputImage.width(), inputImage.height(), 1, 1);
+	CImg< unsigned char > grayImage_gpu = CImg< unsigned char >(inputImage.width(), inputImage.height(), 1, 1);
+
+	rgb2gray(inputImage.data(), grayImage.data(), inputImage.width(), inputImage.height());
+	rgb2grayCuda(inputImage.data(),grayImage_gpu,inputImage.width(), inputImage.height());
+
+	//memory copy from Device to Host
+	/*
+	if(cudaMemcpy(grayImage_gpu,d_output,inputImage.width()*inputImage.height()*sizeof(unsigned char),cudaMemcpyDeviceToHost)
+		!= cudaSuccess){
+		printf("error on memory transfer\n");
+		exit(-3);
+	}
+	*/
+	//CImg< unsigned char > grayImage_gpu = CImg< unsigned char >(buffer[0])
+
+
+	if ( displayImages ) {
+		grayImage.display("Grayscale Image");
+	}
+	if ( saveAllImages ) {
+		grayImage.save("./grayscale.bmp");
+		grayImage_gpu.save("./grayImage_gpu.bmp");
+	}
+
+	// Compute 1D histogram
+	CImg< unsigned char > histogramImage = CImg< unsigned char >(BAR_WIDTH * HISTOGRAM_SIZE, HISTOGRAM_SIZE, 1, 1);
+	unsigned int *histogram = new unsigned int [HISTOGRAM_SIZE];
+
+	histogram1D(grayImage.data(), histogramImage.data(), grayImage.width(), grayImage.height(), histogram, HISTOGRAM_SIZE, BAR_WIDTH);
+	//histogram1DCuda
+
+	if ( displayImages ) {
+		histogramImage.display("Histogram");
+	}
+	if ( saveAllImages ) {
+		histogramImage.save("./histogram.bmp");
+	}
+
+	// Contrast enhancement
+	contrast1D(grayImage.data(), grayImage.width(), grayImage.height(), histogram, HISTOGRAM_SIZE, CONTRAST_THRESHOLD);
+	//contrast1DCuda
+
+	if ( displayImages ) {
+		grayImage.display("Contrast Enhanced Image");
+	}
+	if ( saveAllImages ) {
+		grayImage.save("./contrast.bmp");
+	}
+
+	delete [] histogram;
+
+	// Triangular smooth (convolution)
+	CImg< unsigned char > smoothImage = CImg< unsigned char >(grayImage.width(), grayImage.height(), 1, 1);
+
+	triangularSmooth(grayImage.data(), smoothImage.data(), grayImage.width(), grayImage.height(), filter);
+	//triangularSmoothCuda
+	
+	if ( displayImages ) {
+		smoothImage.display("Smooth Image");
+	}
+	
+	if ( saveAllImages ) {
+		smoothImage.save("./smooth.bmp");
+	}
+	
+
+	return 0;
+}
+
